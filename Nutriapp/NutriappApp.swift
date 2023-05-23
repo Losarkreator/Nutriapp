96ce4f337d54c2211ebadfb89f7997b23d2ab4c9
Losarkreator <losarkreator@gmail.com>
2023-05-23 12:36:32 +0200

    Initial Commit

---
+++ Nutriapp/NutriappApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriappApp.swift
+//  Nutriapp
+//
+//  Created by Losark on 23/5/23.
+//
+
+import SwiftUI
+
+@main
+struct NutriappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
